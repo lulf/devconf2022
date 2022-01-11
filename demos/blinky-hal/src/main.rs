55e13148dd70ad88667869e286a1f0e71f5212a9
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2022-01-11 20:16:16 +0100

    Add blinky hal version

---
+++ demos/blinky-hal/src/main.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use defmt_rtt as _;
+use panic_probe as _;
+
+use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
+use embedded_hal::digital::v2::{InputPin, OutputPin};
+
+#[entry]
+fn main() -> ! {
+    let p = embassy_stm32::init(Default::default());
+    let mut led = Output::new(p.PA5, Level::Low, Speed::VeryHigh);
+    let button = Input::new(p.PC13, Pull::Up);
+
+    loop {
+        if button.is_high().unwrap() {
+            led.set_high().unwrap();
+        } else {
+            led.set_low().unwrap();
+        }
+    }
+}
