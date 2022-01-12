076c0909e2281cadb74f3c33a508d1f1cd852515
Ulf Lilleengen <lulf@redhat.com>
2022-01-12 09:42:57 +0100

    Add toolchain file

--- demos/blinky-hal/src/main.rs
+++ demos/blinky-hal/src/main.rs
@@ -11,14 +11,14 @@ use embedded_hal::digital::v2::{InputPin, OutputPin};
 #[entry]
 fn main() -> ! {
     let p = embassy_stm32::init(Default::default());
-    let mut led = Output::new(p.PA5, Level::Low, Speed::VeryHigh);
+    let mut led = Output::new(p.PB14, Level::High, Speed::VeryHigh);
     let button = Input::new(p.PC13, Pull::Up);
 
     loop {
         if button.is_high().unwrap() {
-            led.set_high().unwrap();
-        } else {
             led.set_low().unwrap();
+        } else {
+            led.set_high().unwrap();
         }
     }
 }
