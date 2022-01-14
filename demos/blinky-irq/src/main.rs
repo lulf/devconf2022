03368c5c281749545be469f0afb0c14d33d65109
Ulf Lilleengen <lulf@redhat.com>
2022-01-14 14:14:00 +0100

    Fix IRQ app

--- demos/blinky-irq/src/main.rs
+++ demos/blinky-irq/src/main.rs
@@ -4,6 +4,8 @@
 use defmt_rtt as _;
 use panic_probe as _;
 
+use core::cell::RefCell;
+use cortex_m::interrupt::Mutex;
 use cortex_m::peripheral::NVIC;
 use cortex_m_rt::entry;
 use embassy_stm32::{
@@ -13,71 +15,114 @@ use embassy_stm32::{
 };
 use embedded_hal::digital::v2::{InputPin, OutputPin};
 
-const BUTTON_PIN: usize = 13;
-const BUTTON_PORT: u8 = 2;
-
-static mut BUTTON: Option<Input<'static, PC13>> = None;
-static mut LED: Option<Output<'static, PB14>> = None;
+static BUTTON: Mutex<RefCell<Option<Input<'static, PC13>>>> = Mutex::new(RefCell::new(None));
+static LED: Mutex<RefCell<Option<Output<'static, PB14>>>> = Mutex::new(RefCell::new(None));
 
 #[entry]
 fn main() -> ! {
     let p = embassy_stm32::init(Default::default());
-    let led = p.PB14;
-    let button = p.PC13;
+    let led = Output::new(p.PB14, Level::Low, Speed::Low);
+    let mut button = Input::new(p.PC13, Pull::Up);
 
-    // Setup interrupt
-    cortex_m::interrupt::free(|_| unsafe {
-        LED.replace(Output::new(led, Level::Low, Speed::VeryHigh));
-        BUTTON.replace(Input::new(button, Pull::Up));
+    cortex_m::interrupt::free(|cs| unsafe {
+        enable_interrupt(&mut button);
 
-        NVIC::unpend(pac::Interrupt::EXTI15_10);
+        LED.borrow(cs).borrow_mut().replace(led);
+        BUTTON.borrow(cs).borrow_mut().replace(button);
+
         NVIC::unmask(pac::Interrupt::EXTI15_10);
     });
 
-    enable_button();
-
     loop {
-        //cortex_m::asm::wfe();
+        cortex_m::asm::wfe();
     }
-}
-
-fn enable_button() {
-    let pin = BUTTON_PIN;
-    let port = BUTTON_PORT;
-    let syscfg = pac::SYSCFG;
-    let exti = pac::EXTI;
-    cortex_m::interrupt::free(|_| unsafe {
-        syscfg.exticr(pin / 4).modify(|w| w.set_exti(pin % 4, port));
-        exti.rtsr(0).modify(|w| w.set_line(pin, true));
-        exti.ftsr(0).modify(|w| w.set_line(pin, true));
-
-        exti.pr(0).write(|w| w.set_line(pin, true));
-        exti.imr(0).modify(|w| w.set_line(pin, true));
-    });
 }
 
 #[interrupt]
 fn EXTI15_10() {
+    cortex_m::interrupt::free(|cs| {
+        let mut button = BUTTON.borrow(cs).borrow_mut();
+        let button = button.as_mut().unwrap();
+
+        let mut led = LED.borrow(cs).borrow_mut();
+        let led = led.as_mut().unwrap();
+        if check_interrupt(button) {
+            if button.is_low().unwrap() {
+                led.set_high().unwrap();
+            } else {
+                led.set_low().unwrap();
+            }
+        }
+        clear_interrupt(button);
+    });
+}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// "Hidden" HAL-like methods for doing interrupts with embassy. Hardcode pin just to give audience an idea of what it looks like
+
+const PORT: u8 = 2;
+const PIN: usize = 13;
+fn check_interrupt<P: Pin>(_pin: &mut Input<'static, P>) -> bool {
     let exti = pac::EXTI;
     unsafe {
-        defmt::info!("EXTI15_10 IRQ");
-        let mut lines = exti.pr(0).read();
-        if lines.line(BUTTON_PIN) {
-            defmt::info!("BUTTON PIN LINE SET");
-            let imr = exti.imr(0).read();
-            if !imr.line(BUTTON_PIN) {
-                if BUTTON.as_ref().unwrap().is_low().unwrap() {
-                    LED.as_mut().unwrap().set_high().unwrap();
-                } else {
-                    LED.as_mut().unwrap().set_low().unwrap();
-                }
-                defmt::println!("Interrupt from button");
-                //unsafe { exti.imr(0).modify(|w| w.set_line(13, false)) };
-            }
-            lines.set_line(BUTTON_PIN, false);
-        }
-
-        exti.pr(0).write_value(lines);
-        NVIC::unpend(pac::Interrupt::EXTI15_10);
+        let pin = PIN;
+        let lines = exti.pr(0).read();
+        lines.line(pin)
     }
+}
+
+fn clear_interrupt<P: Pin>(_pin: &mut Input<'static, P>) {
+    let exti = pac::EXTI;
+    unsafe {
+        let pin = PIN;
+        let mut lines = exti.pr(0).read();
+        lines.set_line(pin, true);
+        exti.pr(0).write_value(lines);
+    }
+}
+
+fn enable_interrupt<P: Pin>(_pin: &mut Input<'static, P>) {
+    cortex_m::interrupt::free(|_| unsafe {
+        let rcc = pac::RCC;
+        rcc.apb2enr().modify(|w| w.set_syscfgen(true));
+
+        let port = PORT;
+        let pin = PIN;
+        let syscfg = pac::SYSCFG;
+        let exti = pac::EXTI;
+        syscfg.exticr(pin / 4).modify(|w| w.set_exti(pin % 4, port));
+        exti.imr(0).modify(|w| w.set_line(pin, true));
+        exti.rtsr(0).modify(|w| w.set_line(pin, true));
+        exti.ftsr(0).modify(|w| w.set_line(pin, true));
+    });
 }
